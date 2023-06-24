eb048a987904f7bac86c5e2e95cc8e4d171a2d26
haupham809 <haupham404@gmail.com>
2023-06-24 09:48:04 +0700

    update

--- Chuongtrinh/cronjob/utils/file.go
+++ Chuongtrinh/cronjob/utils/file.go
@@ -150,10 +150,11 @@ func indexAllSubstrings(text, term string) []int {
 	return indexes
 }
 func Clearstring(s string) string {
-	s = strings.ReplaceAll(strings.ReplaceAll(s, "\r", ""), "\n", "")
+	s = strings.ReplaceAll(s, "\r", "")
 
 	return s
 }
+
 func HighlightWords(inputPath, outputPath string, resultcaudaovan []object.Detaildaovan) error {
 	reader, f, err := model.NewPdfReaderFromFile(inputPath, nil)
 	if err != nil {
@@ -166,6 +167,7 @@ func HighlightWords(inputPath, outputPath string, resultcaudaovan []object.Detai
 		return err
 	}
 	cr := creator.New()
+	color := make(map[string]string)
 	for n := 1; n <= numPages; n++ {
 		page, err := reader.GetPage(n)
 		if err != nil {
@@ -176,36 +178,45 @@ func HighlightWords(inputPath, outputPath string, resultcaudaovan []object.Detai
 		}
 		for i, value := range resultcaudaovan {
 			if value.Percent > 0 {
+				if color[value.Locationfilecompare] == "" {
+					color[value.Locationfilecompare] = "#FFFF00"
+				}
 				term := Clearstring(value.Keywor2)
-				bBoxes, err := getBoundingBoxes(page, term)
-				if err != nil {
-					return err
-				}
-
-				mediaBox, err := page.GetMediaBox()
-				if err != nil {
-					return err
-				}
-				if page.MediaBox == nil {
-					page.MediaBox = mediaBox
-				}
-
-				h := mediaBox.Ury
-				for _, bbox := range bBoxes {
-					rect := cr.NewRectangle(bbox.Llx, h-bbox.Lly, bbox.Urx-bbox.Llx, -(bbox.Ury - bbox.Lly))
-					rect.SetFillColor(creator.ColorRGBFromHex("#FFFF00"))
-					rect.SetBorderWidth(0)
-					rect.SetFillOpacity(0.5)
-					if err := cr.Draw(rect); err != nil {
-						return nil
+				checkboxes := false
+				for _, v := range strings.Split(term, "\n\n") {
+					term = v
+					bBoxes, err := getBoundingBoxes(page, term)
+					if err != nil {
+						return err
 					}
 
-				}
-				if len(bBoxes) > 0 {
-					if i < len(resultcaudaovan)-1 {
-						resultcaudaovan = RemoveValueForObject(resultcaudaovan, i)
+					mediaBox, err := page.GetMediaBox()
+					if err != nil {
+						return err
+					}
+					if page.MediaBox == nil {
+						page.MediaBox = mediaBox
+					}
+
+					h := mediaBox.Ury
+					for _, bbox := range bBoxes {
+						rect := cr.NewRectangle(bbox.Llx, h-bbox.Lly, bbox.Urx-bbox.Llx, -(bbox.Ury - bbox.Lly))
+						rect.SetFillColor(creator.ColorRGBFromHex(color[value.Locationfilecompare]))
+						rect.SetBorderWidth(0)
+						rect.SetFillOpacity(0.5)
+						if err := cr.Draw(rect); err != nil {
+							return nil
+						}
 
 					}
+					if len(bBoxes) > 0 {
+
+						checkboxes = true
+					}
+				}
+				if i < len(resultcaudaovan)-1 && checkboxes {
+					resultcaudaovan = RemoveValueForObject(resultcaudaovan, i)
+
 				}
 			}
 		}
